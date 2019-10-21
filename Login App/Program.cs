f3d3d44151697db0c73f867d259d160bbd4f5dea
Denis J Finkel <46610866+JFinkel7@users.noreply.github.com>
2019-10-21 11:09:48 -0500

    Added AzureKeyVault Statements

--- Login App/Program.cs
+++ Login App/Program.cs
@@ -1,21 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.Services.AppAuthentication;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
-
+using Microsoft.Extensions.Configuration.AzureKeyVault;
+//
 namespace Login_App {
     public class Program {
+        private static string GetKeyVaultEndpoint() => "https://vaultcustomerinformation.vault.azure.net";
         public static void Main(string[] args) {
-            CreateWebHostBuilder(args).Build().Run();
+            BuildWebHost(args).Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHost BuildWebHost(string[] args) =>
+           WebHost.CreateDefaultBuilder(args)
+               .ConfigureAppConfiguration((ctx, builder) => {
+                   var keyVaultEndpoint = GetKeyVaultEndpoint();
+                   if (!string.IsNullOrEmpty(keyVaultEndpoint)) {
+                       var azureServiceTokenProvider = new AzureServiceTokenProvider();
+                       var keyVaultClient = new KeyVaultClient(
+                           new KeyVaultClient.AuthenticationCallback(
+                               azureServiceTokenProvider.KeyVaultTokenCallback));
+                       builder.AddAzureKeyVault(
+                           keyVaultEndpoint, keyVaultClient, new DefaultKeyVaultSecretManager());
+                   }
+               }).UseStartup<Startup>().Build();
     }
 }
